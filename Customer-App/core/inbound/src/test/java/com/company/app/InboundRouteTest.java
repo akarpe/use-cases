b736fe9d8f983f850503ddce0185d354d92950a7
Krystal Ying <kying@KMA.local>
2015-12-16 11:11:23 -0500

    set content type

--- Customer-App/core/inbound/src/test/java/com/company/app/InboundRouteTest.java
+++ Customer-App/core/inbound/src/test/java/com/company/app/InboundRouteTest.java
@@ -5,11 +5,16 @@ import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 
 
 import javax.ws.rs.core.Response;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 import static org.junit.Assert.*;
 
@@ -47,12 +52,31 @@ public class InboundRouteTest extends CamelBlueprintTestSupport {
 
         httpClient = HttpClientBuilder.create().build();
         HttpPost httpPost = new HttpPost("http://localhost:8181/cxf/myService/deim/add");
+        StringEntity input = new StringEntity(getSampleMessage("PatientDemographics.xml"));
+        input.setContentType("application/xml");
+        httpPost.setEntity(input);
         HttpResponse httpResponse = httpClient.execute(httpPost);
 
 
     }
 
+    private String getSampleMessage(String pFileName) throws Exception {
+        StringBuffer sb = new StringBuffer(1024);
 
+        BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/" + pFileName)));
+
+        try {
+            for (String line = input.readLine(); line != null; line = input.readLine()) {
+                sb.append(line);
+            }
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+        }
+
+        return sb.toString();
+    }
 
 
 

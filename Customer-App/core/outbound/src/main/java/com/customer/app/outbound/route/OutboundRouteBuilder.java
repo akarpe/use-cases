025f867278ce6d67f2198f60ff4a414e8964cb25
Balazs Szeti <bszeti@localhost.localdomain>
2015-12-17 18:01:58 -0500

    CXF WS call with dataFormat=POJO is added

--- Customer-App/core/outbound/src/main/java/com/customer/app/outbound/route/OutboundRouteBuilder.java
+++ Customer-App/core/outbound/src/main/java/com/customer/app/outbound/route/OutboundRouteBuilder.java
@@ -4,19 +4,19 @@ import java.net.ConnectException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.ws.Holder;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.cxf.common.message.CxfConstants;
-import org.apache.camel.dataformat.soap.name.ServiceInterfaceStrategy;
 import org.apache.camel.model.dataformat.JaxbDataFormat;
-import org.apache.camel.model.dataformat.SoapJaxbDataFormat;
 import org.apache.cxf.binding.soap.SoapFault;
+import org.apache.cxf.message.MessageContentsList;
 
 import com.sun.mdm.index.webservice.ExecuteMatchUpdate;
 import com.sun.mdm.index.webservice.ExecuteMatchUpdateResponse;
-import com.sun.mdm.index.webservice.PersonEJB;
-import com.sun.mdm.index.webservice.PersonEJBService;
+import com.sun.mdm.index.webservice.MatchColResult;
 
 public class OutboundRouteBuilder extends RouteBuilder {
 
@@ -35,10 +35,9 @@ public class OutboundRouteBuilder extends RouteBuilder {
 		  .to("log:ConnectException?showAll=true&multiline=true")
 		  .inOnly("mq:q.empi.nextgate.dlq");
 		
+		//Send back something in case of Fault to have a reply
 		onException(SoapFault.class)
 		  .handled(true)
-		  //.useOriginalMessage()
-		  //.convertBodyTo(String.class)
 		  .to("log:SoapFault?showAll=true&multiline=true")
 		  .inOnly("mq:q.empi.nextgate.dlq")
 		  .setBody(property(Exchange.EXCEPTION_CAUGHT))
@@ -66,14 +65,15 @@ public class OutboundRouteBuilder extends RouteBuilder {
 		  .convertBodyTo(String.class) //Not needed, but verifies response
 		  .to("log:outboundRoute4?showAll=true&multiline=true")
 		; 
-/*		
+
+		 
+		 /*		
         //This uses dataFormat=MESSAGE, but it doesn't work in Fuse6.1 (works in Fuse 6.2.1, but needs to add to 'org.xmlsoap.schemas.soap.envelope' to Import-Package
 		SoapJaxbDataFormat soap = new SoapJaxbDataFormat("com.sun.mdm.index.webservice", new ServiceInterfaceStrategy(PersonEJB.class,true) );
 		
 		from("mq:q.empi.nextgate.out")
 		  .unmarshal(jaxbPersonService)
 		  .convertBodyTo(ExecuteMatchUpdate.class)
-//		  .process(prepareWsCall)
 		  .to("log:outboundRouteA?showAll=true&multiline=true")
 		  .marshal(soap)
 		  .to("log:outboundRouteB?showAll=true&multiline=true")
@@ -81,58 +81,63 @@ public class OutboundRouteBuilder extends RouteBuilder {
 		  
 		  .setHeader(CxfConstants.OPERATION_NAME,constant("executeMatchUpdate"))
 		  .to("log:outboundRoute1?showAll=true&multiline=true")
-		  //.to("cxf:bean:nextgateEndpoint?dataFormat=POJO&loggingFeatureEnabled=true")
 		  .to("cxf:bean:nextgateEndpoint?dataFormat=MESSAGE&loggingFeatureEnabled=true")
 		  .to("log:outboundRoute2?showAll=true&multiline=true")
-		  //.log("LIST SIZE: ${body.size}")
-		  //.setBody(simple("${body[0]}"))
-		  //.convertBodyTo(ExecuteMatchUpdateResponse.class)
-		   .convertBodyTo(String.class)
+		  .convertBodyTo(String.class)
 		  .to("log:outboundRoute3?showAll=true&multiline=true")
 		  .unmarshal(jaxbPersonService)
 		  .to("log:outboundRoute4?showAll=true&multiline=true")
 		  .marshal(jaxbPersonService)
 		  .convertBodyTo(String.class)
-		  
-		  //.setBody(simple("<responseSoap>OKOK</responseSoap>"));
 		;		
 		
 		*/
 		 
 		 //This uses dataFormat=POJO
-//			
-//			Processor prepareWsCall = new Processor() {
-//				@Override
-//				public void process(Exchange exchange) throws Exception {
-//					ExecuteMatchUpdate o = (ExecuteMatchUpdate) exchange.getIn().getBody();
-//					List list = new ArrayList();
-//					list.add(o.getCallerInfo());
-//					list.add(o.getSysObjBean());
-//					exchange.getIn().setBody(list);
-//				}
-//			};
+			
+			Processor prepareWsCall = new Processor() {
+				@Override
+				public void process(Exchange exchange) throws Exception {
+					ExecuteMatchUpdate o = (ExecuteMatchUpdate) exchange.getIn().getBody();
+					List list = new ArrayList();
+					list.add(o.getCallerInfo());
+					list.add(o.getSysObjBean());
+					exchange.getIn().setBody(list);
+				}
+			};
+			
+			Processor prepareWsResponse = new Processor() {
+				@Override
+				public void process(Exchange exchange) throws Exception {
+					MessageContentsList list = (MessageContentsList) exchange.getIn().getBody();
+					
+					//MatchColResult is the <return> in the soap response
+					MatchColResult result = (MatchColResult) list.get(0);
+					
+					//It doesn't make sense to create a ExecuteMatchUpdateResponse here, but that's returned in case of dataFormat=PAYLOAD mode				
+					ExecuteMatchUpdateResponse response = new ExecuteMatchUpdateResponse();
+					response.setReturn(result);
+					exchange.getIn().setBody(response);
+				}
+			};
 		 
 		
-/*		
-		from("mq:q.empi.nextgate.out")
-//		  .unmarshal(jaxbPersonService)
-//		  .convertBodyTo(ExecuteMatchUpdate.class)
-//		  .process(prepareWsCall)
-		  .convertBodyTo(String.class)
+		
+		from("mq:q.empi.nextgate.out-POJO")
+		  .unmarshal(jaxbPersonService)
+		  .convertBodyTo(ExecuteMatchUpdate.class)
+		  .process(prepareWsCall)
 		  .setHeader("operationName",constant("executeMatchUpdate"))
-		  .to("log:outboundRoute?showAll=true&multiline=true")
-		  //.to("cxf:bean:nextgateEndpoint?dataFormat=POJO&loggingFeatureEnabled=true")
-		  .to("cxf:bean:nextgateEndpoint?dataFormat=PAYLOAD&loggingFeatureEnabled=true")
-		  .to("log:outboundRoute?showAll=true&multiline=true")
-		  //.log("LIST SIZE: ${body.size}")
-		  //.setBody(simple("${body[0]}"))
-		  //.convertBodyTo(ExecuteMatchUpdateResponse.class)
-		  .to("log:outboundRoute?showAll=true&multiline=true")
-		  //.marshal(jaxbPersonService)
+		  .to("log:outboundRoute1?showAll=true&multiline=true")
 		  
-		  //.setBody(simple("<responseSoap>OKOK</responseSoap>"));
+		  .to("cxf:bean:nextgateEndpoint?dataFormat=POJO&loggingFeatureEnabled=true")
+		  
+		  .to("log:outboundRoute3?showAll=true&multiline=true")
+		  .process(prepareWsResponse)
+		  .marshal(jaxbPersonService)
+		  .convertBodyTo(String.class)
+		  .to("log:outboundRoute4?showAll=true&multiline=true")
 		;
-*/
 
 	}
 
